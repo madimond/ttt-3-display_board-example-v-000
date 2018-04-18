c26b9bdb08c1c37896e60c229e523e1748ba0453
Margaret Dimond <margaretdimond@gmail.com>
2018-04-18 21:24:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,31 +1,30 @@
 def display_board
-
-	board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+	cell = " "
 
 	#Position 1: Left top
-	board[0] = " "
+	cell[0] = " "
 	# Position 2: Middle top
-	board[1] = " "
+	cell[1] = " "
 	# Position 3: Right top
-	board[2] = " "
+	cell[2] = " "
 
 	# Position 4: Left middle
-	board[3] = " "
+	cell[3] = " "
 	# Position 5: Middle middle
-	board[4] = " "
+	cell[4] = " "
 	# Position 6: Right middle
-	board[5] = " "
+	cell[5] = " "
 
 	# Position 7: Left bottom
-	board[6] = " "
+	cell[6] = " "
 	# Position 8: Middle bottom
-	board[7] = " "
+	cell[7] = " "
 	# Position 9: Right bottom
-	board[8] = " "
+	cell[8] = " "
 
-	puts "#{board[0]} | #{board[1]} | #{board[2]}"
+	puts cell[0] + "|" + cell[1] + "|" + cell[2]
 	puts "-----------"
-	puts "#{board[3]} | #{board[4]} | #{board[5]}"
+	puts cell[3] + "|" + cell[4] + "|" + cell[5]
 	puts "-----------"
-	puts "#{board[6]} | #{board[7]} | #{board[8]}"
+	puts cell[6] + "|" + cell[7] + "|" + cell[8]
 end
