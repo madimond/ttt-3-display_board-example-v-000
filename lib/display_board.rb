0a4368116636205e2900062526d3263413cf4390
Margaret Dimond <margaretdimond@gmail.com>
2018-04-18 21:41:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,30 +1,8 @@
 def display_board
 	cell = "   "
-
-	#Position 1: Left top
-	cell[0] = "   "
-	# Position 2: Middle top
-	cell[1] = "   "
-	# Position 3: Right top
-	cell[2] = "   "
-
-	# Position 4: Left middle
-	cell[3] = "   "
-	# Position 5: Middle middle
-	cell[4] = "   "
-	# Position 6: Right middle
-	cell[5] = "   "
-
-	# Position 7: Left bottom
-	cell[6] = "   "
-	# Position 8: Middle bottom
-	cell[7] = "   "
-	# Position 9: Right bottom
-	cell[8] = "   "
-
-	puts cell[0] + "|" + cell[1] + "|" + cell[2]
+	puts "#{cell}|#{cell}|#{cell}"
 	puts "-----------"
-	puts cell[3] + "|" + cell[4] + "|" + cell[5]
+	puts "#{cell}|#{cell}|#{cell}"
 	puts "-----------"
-	puts cell[6] + "|" + cell[7] + "|" + cell[8]
+	puts "#{cell}|#{cell}|#{cell}"
 end
